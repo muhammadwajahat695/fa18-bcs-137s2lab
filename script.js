7b76ab06f161f08f949836e6e8ffb9bd78ecb372
wajahat <muhammadwajahat695@gmail.com>
2021-05-18 16:39:29 +0500

    1

--- script.js
+++ script.js
@@ -1,48 +1,82 @@
-jQuery(document).ready(function ($) {
-
-
-    $.ajax({
-        type: "GET",
-        url: "https://reqres.in/api/users?page=" + 1,
-
-        success: function (res) {
-            $('.Container').html('');
-            for (var a = 0; a < res.data.length; a++) {
-                $('.Container').append(' <div class="single-data"> <p class="avatar"> <img src="' + res.data[a].avatar + '" alt=""> </p> <p class="id">' + res.data[a].id + '</p> <p class="firstname ">' + res.data[a].first_name + '</p> <p class="lastname ">' + res.data[a].last_name + '</p> <p class="email">' + res.data[a].email + '</p> </div>');
-            }
-
-        }
-    });
-
-
-
-    $('.Two').click(function () {
-        $.ajax({
-            type: "GET",
-            url: "https://reqres.in/api/users?page=" + 2,
-
-            success: function (res) {
-                $('.Container').html('');
-                for (var a = 0; a < res.data.length; a++) {
-                    $('.Container').append(' <div class="single-data"> <p class="avatar"> <img src="' + res.data[a].avatar + '" alt=""> </p> <p class="id">' + res.data[a].id + '</p> <p class="firstname ">' + res.data[a].first_name + '</p> <p class="lastname ">' + res.data[a].last_name + '</p> <p class="email">' + res.data[a].email + '</p> </div>');
-                }
-
-            }
-        });
-    });
-
-    $('.ONE').click(function () {
-        $.ajax({
-            type: "GET",
-            url: "https://reqres.in/api/users?page=" + 1,
-
-            success: function (res) {
-                $('.Container').html('');
-                for (var a = 0; a < res.data.length; a++) {
-                    $('.Container').append(' <div class="single-data"> <p class="avatar"> <img src="' + res.data[a].avatar + '" alt=""> </p> <p class="id">' + res.data[a].id + '</p> <p class="firstname ">' + res.data[a].first_name + '</p> <p class="lastname ">' + res.data[a].last_name + '</p> <p class="email">' + res.data[a].email + '</p> </div>');
-                }
-
-            }
-        });
-    });
+$(function () {
+  load();
+  $("#Two").click(Two);
+  $("#ONE").click(ONE);
 });
+function load() {
+  $.ajax({
+    type: "GET",
+    url: "https://reqres.in/api/users?page=" + 1,
+
+    success: function (response) {
+      $("#Container").html("");
+      for (var a = 0; a < response.data.length; a++) {
+        $("#Container").append(
+          ' <div class="single-data"> <p class="avatar"> <img src="' +
+            response.data[a].avatar +
+            '" alt=""> </p> <p class="id">' +
+            response.data[a].id +
+            '</p> <p class="firstname ">' +
+            response.data[a].first_name +
+            '</p> <p class="lastname ">' +
+            response.data[a].last_name +
+            '</p> <p class="email">' +
+            response.data[a].email +
+            "</p> </div>"
+        );
+      }
+    },
+  });
+}
+
+function Two() {
+  $.ajax({
+    type: "GET",
+    url: "https://reqres.in/api/users?page=" + 2,
+
+    success: function (response) {
+      $("#Container").html("");
+      for (var a = 0; a < response.data.length; a++) {
+        $("#Container").append(
+          ' <div class="single-data"> <p class="avatar"> <img src="' +
+            response.data[a].avatar +
+            '" alt=""> </p> <p class="id">' +
+            response.data[a].id +
+            '</p> <p class="firstname ">' +
+            response.data[a].first_name +
+            '</p> <p class="lastname ">' +
+            response.data[a].last_name +
+            '</p> <p class="email">' +
+            response.data[a].email +
+            "</p> </div>"
+        );
+      }
+    },
+  });
+}
+
+function ONE() {
+  $.ajax({
+    type: "GET",
+    url: "https://reqres.in/api/users?page=" + 1,
+
+    success: function (response) {
+      $("#Container").html("");
+      for (var a = 0; a < response.data.length; a++) {
+        $("#Container").append(
+          ' <div class="single-data"> <p class="avatar"> <img src="' +
+            response.data[a].avatar +
+            '" alt=""> </p> <p class="id">' +
+            response.data[a].id +
+            '</p> <p class="firstname ">' +
+            response.data[a].first_name +
+            '</p> <p class="lastname ">' +
+            response.data[a].last_name +
+            '</p> <p class="email">' +
+            response.data[a].email +
+            "</p> </div>"
+        );
+      }
+    },
+  });
+}
